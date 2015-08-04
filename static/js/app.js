71609483768c2bfd593fde39d7bd1ffe3ada3984
Scott Doxey <neogeek0101@gmail.com>
2015-08-04 19:46:10 -0400

    Whitespace.

--- static/js/app.js
+++ static/js/app.js
@@ -1,120 +1,120 @@
 (function () {
 
-	'use strict';
+    'use strict';
 
-	/*global window, document, confirm, $, Handlebars */
+    /*global window, document, confirm, $, Handlebars */
 
-	var html = document.querySelector('html'),
-		feed = document.querySelector('.feed'),
-		config = null;
+    var html = document.querySelector('html'),
+        feed = document.querySelector('.feed'),
+        config = null;
 
-	function reset_all_config() {
+    function reset_all_config() {
 
-		window.localStorage.setItem('config', JSON.stringify({ read: [] }));
+        window.localStorage.setItem('config', JSON.stringify({ read: [] }));
 
-	}
+    }
 
-	function hide_read_stories(data) {
+    function hide_read_stories(data) {
 
-		var key;
+        var key;
 
-		for (key in data.stories) {
+        for (key in data.stories) {
 
-			if (data.stories.hasOwnProperty(key)) {
+            if (data.stories.hasOwnProperty(key)) {
 
-				if (config.read.indexOf(data.stories[key].hash) !== -1) {
-					data.stories[key] = [];
-					data.count--;
-				}
+                if (config.read.indexOf(data.stories[key].hash) !== -1) {
+                    data.stories[key] = [];
+                    data.count--;
+                }
 
-			}
+            }
 
-		}
+        }
 
-		return data;
+        return data;
 
-	}
+    }
 
-	function mark_story_as_read(hash) {
+    function mark_story_as_read(hash) {
 
-		if (config.read.indexOf(hash) === -1) {
-			config.read.push(hash);
-		}
+        if (config.read.indexOf(hash) === -1) {
+            config.read.push(hash);
+        }
 
-		window.localStorage.setItem('config', JSON.stringify(config));
+        window.localStorage.setItem('config', JSON.stringify(config));
 
-	}
+    }
 
-	if (!window.localStorage.getItem('config')) {
+    if (!window.localStorage.getItem('config')) {
 
-		reset_all_config();
+        reset_all_config();
 
-	}
+    }
 
-	config = JSON.parse(window.localStorage.getItem('config'));
+    config = JSON.parse(window.localStorage.getItem('config'));
 
-	html.setAttribute('class', 'loading');
+    html.setAttribute('class', 'loading');
 
-	setTimeout(function () {
+    setTimeout(function () {
 
-		$.getJSON('/feeds').done(function (data) {
+        $.getJSON('/feeds').done(function (data) {
 
-			var template;
+            var template;
 
-			html.removeAttribute('class');
+            html.removeAttribute('class');
 
-			data = hide_read_stories(data);
+            data = hide_read_stories(data);
 
-			template = Handlebars.compile(data.template);
+            template = Handlebars.compile(data.template);
 
-			feed.innerHTML = template(data);
+            feed.innerHTML = template(data);
 
-		}).fail(function () {
+        }).fail(function () {
 
-			html.removeAttribute('class');
+            html.removeAttribute('class');
 
-			feed.innerHTML = '<p>Error processing request.</p>';
+            feed.innerHTML = '<p>Error processing request.</p>';
 
-		});
+        });
 
-	}, 10);
+    }, 10);
 
-	$(document).on('click', 'a[href="#nav"]', function (e) {
+    $(document).on('click', 'a[href="#nav"]', function (e) {
 
-		e.preventDefault();
+        e.preventDefault();
 
-		$('.nav ul').toggle();
+        $('.nav ul').toggle();
 
-	}).on('click', 'a[href="#reload"]', function (e) {
+    }).on('click', 'a[href="#reload"]', function (e) {
 
-		e.preventDefault();
+        e.preventDefault();
 
-		window.location.reload();
+        window.location.reload();
 
-	}).on('click', 'a[href="#markallasread"]', function (e) {
+    }).on('click', 'a[href="#markallasread"]', function (e) {
 
-		e.preventDefault();
+        e.preventDefault();
 
-		$('.story').each(function (key, value) {
+        $('.story').each(function (key, value) {
 
-			mark_story_as_read(value.getAttribute('data-hash'));
+            mark_story_as_read(value.getAttribute('data-hash'));
 
-		});
+        });
 
-		window.location.reload();
+        window.location.reload();
 
-	}).on('click', 'a[href="#resetreadstories"]', function (e) {
+    }).on('click', 'a[href="#resetreadstories"]', function (e) {
 
-		e.preventDefault();
+        e.preventDefault();
 
-		if (confirm('Reset read stories?')) {
+        if (confirm('Reset read stories?')) {
 
-			reset_all_config();
+            reset_all_config();
 
-			window.location.reload();
+            window.location.reload();
 
-		}
+        }
 
-	});
+    });
 
 }());
